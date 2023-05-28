2c04e8bb4c9764c1b542d9f193e662494a747d70
oobayouzou330 <134581375+oobayouzou330@users.noreply.github.com>
2023-05-28 14:30:42 +0900

    Update hhhhhhhhh.java
    等待pull的2文件

--- src/sdfgsgs/hhhhhhhhh.java
+++ src/sdfgsgs/hhhhhhhhh.java
@@ -5,6 +5,8 @@ public class hhhhhhhhh {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("这是一个主干程序，命名为1");
+		System.out.println("这是一个等待pull的程序，命名为2");
+		
 	}
 
 }
