6853609365a325844ebee6176cc96d0e323ba117
oobayouzou330 <bayou-04-sunbeam@icloud.com>
2023-05-28 14:31:46 +0900

    这是一个测试文件，用来测试pull

--- src/sdfgsgs/hhhhhhhhh.java
+++ src/sdfgsgs/hhhhhhhhh.java
@@ -5,6 +5,7 @@ public class hhhhhhhhh {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("这是一个主干程序，命名为1");
+		System.out.println("这是一个计划更新的文件3，用以测试pull和push");
 	}
 
 }
